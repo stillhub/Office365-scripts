0092480804d494a14cf9e298bd9b1f667248ffb0
Jared Stillwell <75730966+stillhub@users.noreply.github.com>
2022-12-05 16:17:10 +0000

    Added delegate calendar permissions in O365 script

---
+++ DelegateCalendarPermissions/CalPermissions.ps1
@@ -0,0 +1,16 @@
+﻿# Example use case for adding, setting and getting Mailbox folder permissions for Office 365.
+# Script created by Jared Stillwell
+# Version: 1.0
+#  Notes:
+#
+
+import-Module ExchangeOnlineManagement
+
+Connect-ExchangeOnline
+
+
+Add-MailboxFolderPermission -Identity x@example.com:\Calendar -User y@example.com -AccessRights Editor -SharingPermissionFlags Delegate
+
+Set-MailboxFolderPermission -Identity x@example.com:\Calendar -User default -AccessRights Reviewer
+
+Get-MailboxFolderPermission -Identity x@example.com:\Calendar
