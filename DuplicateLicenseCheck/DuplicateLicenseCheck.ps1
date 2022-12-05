0f0c93ebee9d549189f698d2163878907f72df8f
Jared Stillwell <75730966+stillhub@users.noreply.github.com>
2022-12-05 16:24:44 +0000

    Script to check licenses of O365 users.
    Uploaded script to check if any users have two specific licenses assigned.

---
+++ DuplicateLicenseCheck/DuplicateLicenseCheck.ps1
@@ -0,0 +1,24 @@
+﻿# Script to create a report with users that have SKU license A ($SKU_A) and SKU license B ($SKU_B).
+# Script created by Jared Stillwell
+# Version: 1.0
+#  Notes:
+#   - Change $SKU_A and $SKU_B values to SKU values you would like to compare.
+
+#Get-MsolAccountSku
+
+Connect-Msolservice
+
+$SKU_A = "example:SPE_E3"
+$SKU_B = "example:SPE_E5"
+
+$LogLocation = ".\Log.csv"
+
+$DuplicatedLicenses = Get-MsolUser -All | Where-Object {($_.licenses).AccountSkuId -match $SKU_A -and ($_.licenses).AccountSkuId -match $SKU_B}
+$DuplicatedLicenses | Select-Object DisplayName,UserPrincipalName | Export-CSV -Path $LogLocation -NoTypeInformation
+
+if(($DuplicatedLicenses)){
+    Write-host "TRUE"
+
+}else{
+    Write-host "FALSE"
+}
