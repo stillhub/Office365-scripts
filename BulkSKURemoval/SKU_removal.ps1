dc183ca461f06a76ef593a738d3bd20cbec543de
Jared Stillwell <75730966+stillhub@users.noreply.github.com>
2022-12-05 16:13:57 +0000

    Added Bulk License SKU remover script

---
+++ BulkSKURemoval/SKU_removal.ps1
@@ -0,0 +1,20 @@
+﻿# Script that removes specified license SKU from multiple Office 365 accounts.
+# Script created by Jared Stillwell
+# Version: 1.0
+#  Notes:
+#   -Adjust $RemoveSKU to specific license SKU.
+#   -Add UPNs to 'UserUPNs.txt'
+#
+
+Import-Module MSOnline
+Connect-MsolService
+
+# Get-MsolAccountSku
+
+$RemoveSKU = "example:THREAT_INTELLIGENCE"
+
+$UPNList = Get-Content -Path ".\UserUPNs.txt"
+
+foreach($User in $UPNList){
+    Set-MsolUserLicense -UserPrincipalName $User -RemoveLicenses $RemoveSKU
+}
