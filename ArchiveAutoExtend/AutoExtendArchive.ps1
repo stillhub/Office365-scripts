7712941db0b170af9101f2747bf3a73ef7d8f39a
Jared Stillwell <75730966+stillhub@users.noreply.github.com>
2022-12-05 16:30:34 +0000

    Added script to add auto extend of archive

---
+++ ArchiveAutoExtend/AutoExtendArchive.ps1
@@ -0,0 +1,19 @@
+﻿# Script to enable Auto expanding archive for O365 user
+# Script created by Jared Stillwell
+# Version: 1.0
+#  Notes:
+#   -
+#
+
+
+$EmailOfUser = "x@example.com"
+
+##############################################
+
+Import-Module ExchangeOnlineManagement
+
+Connect-ExchangeOnline
+
+Enable-Mailbox $EmailOfUser -AutoExpandingArchive
+
+Get-Mailbox $EmailOfUser | Format-list
